3c46949597d49110f6affd019b22066dbef691ba
unix <unix.bio@gmail.com>
2020-03-19 23:09:45 +0800

    feat(radio): add component

---
+++ components/radio/radio-group.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useMemo, useState } from 'react'
+import withDefaults from '../utils/with-defaults'
+import useTheme from '../styles/use-theme'
+import { RadioContext } from './radio-context'
+
+interface Props {
+  value: string
+  initialValue?: string
+  disabled?: boolean
+  onChange?: (value: string) => void
+  className?: string
+  useRow?: boolean
+}
+
+const defaultProps = {
+  disabled: false,
+  className: '',
+  useRow: false,
+}
+
+export type RadioGroupProps = Props & typeof defaultProps & React.HTMLAttributes<any>
+
+const RadioGroup: React.FC<React.PropsWithChildren<RadioGroupProps>> = React.memo(({
+  disabled, onChange, value, children, className, initialValue, useRow, ...props
+}) => {
+  const theme = useTheme()
+  const [selfVal, setSelfVal] = useState<string | undefined>(initialValue)
+
+  const updateState = (nextValue: string) => {
+    setSelfVal(nextValue)
+    onChange && onChange(nextValue)
+  }
+
+  const providerValue = useMemo(() => {
+    return {
+      updateState,
+      disabledAll: disabled,
+      inGroup: true,
+      value: selfVal,
+    }
+  },[disabled, selfVal])
+  
+  useEffect(() => {
+    setSelfVal(value)
+  }, [value])
+
+  return (
+    <RadioContext.Provider value={providerValue}>
+      <div className={`radio-group ${className}`} {...props}>
+        {children}
+      </div>
+      <style jsx>{`
+        .radio-group {
+          display: flex;
+          flex-direction: ${useRow ? 'col' : 'column'};
+        }
+        
+        .radio-group :global(.radio) {
+          margin-top: ${useRow ? 0 : theme.layout.gap};
+          margin-left: ${useRow ? theme.layout.gap : 0};
+        }
+        
+        .radio-group :global(.radio:first-of-type) {
+          margin: 0;
+        }
+      `}</style>
+    </RadioContext.Provider>
+  )
+})
+
+export default withDefaults(RadioGroup, defaultProps)
--- lib/components/attributes/attributes-title.tsx
+++ lib/components/attributes/attributes-title.tsx
@@ -2,15 +2,37 @@ import React from 'react'
 import { Spacer, Code } from 'components'
 
 export interface AttributesTitleProps {
+  alias?: string
+}
+
+const getAlias = (alias?: string) => {
+  if (!alias) return null
+  return (
+    <small>[alias: <Code>{alias}</Code>]</small>
+  )
 }
 
 const AttributesTitle: React.FC<React.PropsWithChildren<AttributesTitleProps>> = React.memo(({
-  children,
+  children, alias,
 }) => {
   return (
     <>
-      <h4 className="title"><Code>{children}</Code></h4>
+      <h4 className="title"><Code>{children}</Code>{getAlias(alias)}</h4>
       <Spacer y={.6} />
+  
+      <style jsx>{`
+        h4 {
+          display: inline-flex;
+          height: 1.6rem;
+          line-height: 1.2;
+          align-items: center;
+        }
+
+        h4 :global(small) {
+          font-size: .75em;
+          padding-left: 1rem;
+        }
+      `}</style>
     </>
   )
 })
