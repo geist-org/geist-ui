cb9a9310c2fcc2c0eb806b4e4ab67db65f1dd72a
unix <unix.bio@gmail.com>
2020-03-23 01:21:54 +0800

    feat(tabs): add component

---
+++ components/tabs/tabs-context.ts
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export interface TabsLabelItem {
+  value: string
+  label: string | React.ReactNode
+  disabled: boolean
+}
+
+export interface TabsConfig {
+  register?: (item: TabsLabelItem) => void
+  currentValue?: string
+  inGroup: boolean
+}
+
+const defaultContext = {
+  inGroup: false,
+}
+
+export const TabsContext = React.createContext<TabsConfig>(defaultContext)
+
+export const useTabsContext = (): TabsConfig => React.useContext<TabsConfig>(TabsContext)
