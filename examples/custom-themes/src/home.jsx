bce4acbc447ad1e91039b269d2b3ef4c1d5a0c71
unix <unix.bio@gmail.com>
2020-04-08 19:29:59 +0800

    docs(examples): add custom-themes sample

---
+++ examples/custom-themes/src/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card, Text, Tag, useTheme } from '@zeit-ui/react'
+
+const Home = () => {
+  const theme = useTheme()
+
+  return (
+    <Card shadow style={{ width: '500px', margin: '100px auto' }}>
+      <Text>Modern and minimalist React UI library.</Text>
+      <Text type={'success'}>Modern and minimalist React UI library. <Tag>{theme.palette.success}</Tag></Text>
+      <Text type={'warning'}>Modern and minimalist React UI library. <Tag>{theme.palette.warning}</Tag></Text>
+      <Text type={'error'}>Modern and minimalist React UI library. <Tag>{theme.palette.error}</Tag></Text>
+      <Text type={'secondary'}>Modern and minimalist React UI library.</Text>
+    </Card>
+  )
+}
+
+export default Home
