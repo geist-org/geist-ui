a4604ef633a7b2995b6608c91dec8fe6b6555aac
unix <unix.bio@gmail.com>
2020-04-09 09:38:58 +0800

    feat(tag): append types to tag component

--- components/tag/tag.tsx
+++ components/tag/tag.tsx
@@ -1,51 +1,97 @@
 import React, { useMemo } from 'react'
 import withDefaults from '../utils/with-defaults'
 import useTheme from '../styles/use-theme'
-import { NormalTypes } from '../utils/prop-types'
-import { ZeitUIThemes } from '../styles/themes'
+import { SnippetTypes } from '../utils/prop-types'
+import { ZeitUIThemesPalette } from '../styles/themes'
 
 interface Props {
-  type?: NormalTypes
+  type?: SnippetTypes
+  invert?: boolean
   className?: string
 }
 
 const defaultProps = {
-  type: 'default' as NormalTypes,
+  type: 'default' as SnippetTypes,
+  invert: false,
   className: '',
 }
 
 type NativeAttrs = Omit<React.HTMLAttributes<any>, keyof Props>
 export type TagProps = Props & typeof defaultProps & NativeAttrs
 
-const getColor = (type: NormalTypes, theme: ZeitUIThemes): string => {
-  const colors: { [key in NormalTypes]?: string } = {
-    default: theme.palette.foreground,
-    success: theme.palette.success,
-    warning: theme.palette.warning,
-    error: theme.palette.error,
+export type TagColors = {
+  color: string
+  bgColor: string
+  borderColor: string
+}
+
+const getColors = (
+  type: SnippetTypes,
+  palette: ZeitUIThemesPalette,
+  invert: boolean,
+) => {
+  const colors: { [key in SnippetTypes]: Pick<TagColors, 'color'> & Partial<TagColors> } = {
+    default: {
+      color: palette.foreground,
+    },
+    success: {
+      color: palette.success,
+    },
+    warning: {
+      color: palette.warning,
+    },
+    error: {
+      color: palette.error,
+    },
+    secondary: {
+      color: palette.secondary,
+    },
+    dark: {
+      color: palette.foreground,
+      bgColor: palette.background,
+    },
+    lite: {
+      color: palette.foreground,
+      bgColor: palette.accents_2,
+    }
   }
-  return colors[type] || colors.default as string
+  const hideBorder = invert || type === 'lite'
+  
+  const cardStyle = {
+    ...colors[type],
+    bgColor: colors[type].bgColor || palette.background,
+    borderColor: hideBorder ? 'transparent' : colors[type].color
+  }
+  
+  return !invert ? cardStyle : {
+    ...cardStyle,
+    color: cardStyle.bgColor,
+    bgColor: cardStyle.color,
+  }
 }
 
 const Tag: React.FC<React.PropsWithChildren<TagProps>> = React.memo(({
-  type, children, className, ...props
+  type, children, className, invert, ...props
 }) => {
   const theme = useTheme()
-  const color = useMemo(() => getColor(type, theme), [type, theme])
+  const { color, bgColor, borderColor } = useMemo(
+    () => getColors(type, theme.palette, invert),
+    [type, theme.palette, invert],
+  )
   
   return (
     <span className={className} {...props}>
       {children}
       <style jsx>{`
         span {
-          color: ${color};
           display: inline-block;
           line-height: .875rem;
           font-size: .875rem;
           height: 1.75rem;
           border-radius: ${theme.layout.radius};
-          border: 1px solid ${theme.palette.accents_2};
-          background-color: ${theme.palette.accents_1};
+          border: 1px solid ${borderColor};
+          background-color: ${bgColor};
+          color: ${color};
           padding: 6px;
         }
       `}</style>
