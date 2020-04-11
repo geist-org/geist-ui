7452fe1279a06515ba1c7c5b09047eb7c8874c3b
unix <unix.bio@gmail.com>
2020-04-11 08:18:24 +0800

    feat(auto-complete): support hide empty search results

--- components/auto-complete/auto-complete-empty.tsx
+++ components/auto-complete/auto-complete-empty.tsx
@@ -3,19 +3,21 @@ import withDefaults from '../utils/with-defaults'
 import AutoCompleteSearch from './auto-complete-searching'
 
 interface Props {
+  hidden?: boolean
   className?: string
 }
 
 const defaultProps = {
+  hidden: false,
   className: '',
 }
 
 export type AutoCompleteEmptyProps = Props & typeof defaultProps & React.HTMLAttributes<any>
 
 const AutoCompleteEmpty: React.FC<React.PropsWithChildren<AutoCompleteEmptyProps>> = ({
-  children, className,
+  children, hidden, className,
 }) => {
-
+  if (hidden) return null
   return <AutoCompleteSearch className={className}>{children}</AutoCompleteSearch>
 }
 
