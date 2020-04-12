4c914bd5b78a1c7571103e16eda1b97a52506e67
unix <unix.bio@gmail.com>
2020-04-12 08:16:12 +0800

    fix(select): limit the maximum height of the dropdown-menu

--- components/select/select-dropdown.tsx
+++ components/select/select-dropdown.tsx
@@ -1,26 +1,42 @@
 import React from 'react'
 import useTheme from '../styles/use-theme'
+import withDefaults from '../utils/with-defaults'
 import { useSelectContext } from './select-context'
 import Dropdown from '../shared/dropdown'
 
 interface Props {
   visible: boolean
+  className?: string
+  dropdownStyle?: object
 }
 
-const SelectDropdown: React.FC<React.PropsWithChildren<Props>> = ({
-  visible, children,
+const defaultProps = {
+  className: '',
+  dropdownStyle: {},
+}
+
+type NativeAttrs = Omit<React.HTMLAttributes<any>, keyof Props>
+export type SelectDropdownProps = Props & typeof defaultProps & NativeAttrs
+
+const SelectDropdown: React.FC<React.PropsWithChildren<SelectDropdownProps>> = ({
+  visible, children, className, dropdownStyle,
 }) => {
   const theme = useTheme()
   const { ref } = useSelectContext()
 
   return (
     <Dropdown parent={ref} visible={visible}>
-      <div className="select-dropdown">
+      <div className={`select-dropdown ${className}`} style={dropdownStyle}>
         {children}
         <style jsx>{`
         .select-dropdown {
           border-radius: ${theme.layout.radius};
           box-shadow: ${theme.expressiveness.shadowLarge};
+          background-color: ${theme.palette.background};
+          max-height: 15rem;
+          overflow-y: auto;
+          overflow-anchor: none;
+          padding: ${theme.layout.gapQuarter} 0;
         }
       `}</style>
       </div>
@@ -28,4 +44,4 @@ const SelectDropdown: React.FC<React.PropsWithChildren<Props>> = ({
   )
 }
 
-export default SelectDropdown
+export default withDefaults(SelectDropdown, defaultProps)
