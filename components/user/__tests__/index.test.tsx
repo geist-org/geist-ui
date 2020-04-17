a8ea32eb5912d720c3c6dd6e8f2575a7c9b09316
unix <unix.bio@gmail.com>
2020-04-17 13:24:20 +0800

    test(user): add testcase

---
+++ components/user/__tests__/index.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { mount, render } from 'enzyme'
+import { User } from '../../index'
+
+describe(' User', () => {
+  it('should render correctly', () => {
+    const wrapper = mount(<User name="witt" />)
+    expect(() => wrapper.unmount()).not.toThrow()
+  })
+  
+  it('should support image and text', () => {
+    const wrapper = render(
+      <div>
+        <User name="witt" text="witt" />
+        <User name="witt" src="https://unix.bio/assets/avatar.png" />
+      </div>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+  it('should render description correctly', () => {
+    const wrapper = mount(<User name="witt">description</User>)
+    expect(wrapper.text().toLowerCase()).toContain('description')
+  })
+  
+  it('should render link on user.link', () => {
+    const wrapper = mount(
+      <User name="witt">
+        <User.Link href="https://twitter.com/echo_witt">twitter</User.Link>
+      </User>
+    )
+    const link = wrapper.find('a')
+    expect(link.length).not.toBe(0)
+  })
+})
