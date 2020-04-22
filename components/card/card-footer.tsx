78a475fd7a3566547c690313ce3fa6f088f2b236
unix <unix.bio@gmail.com>
2020-04-22 09:02:31 +0800

    feat(card): add footer component

---
+++ components/card/card-footer.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import useTheme from '../styles/use-theme'
+import withDefaults from '../utils/with-defaults'
+
+interface Props {
+  disableAutoMargin?: boolean
+  className?: string
+}
+
+const defaultProps = {
+  disableAutoMargin: false,
+  className: '',
+}
+
+type NativeAttrs = Omit<React.HTMLAttributes<any>, keyof Props>
+export type CardFooterProps = Props & typeof defaultProps & NativeAttrs
+
+const CardFooter: React.FC<React.PropsWithChildren<CardFooterProps>> = ({
+  children, className, disableAutoMargin, ...props
+}) => {
+  const theme = useTheme()
+
+  return (
+    <footer className={`${disableAutoMargin ? '' : 'auto-margin'} ${className}`}
+      {...props}>
+      {children}
+      <style jsx>{`
+        footer {
+          padding: ${theme.layout.gapHalf} ${theme.layout.gap};
+          display: flex;
+          align-items: center;
+          overflow: hidden;
+          color: inherit;
+          background-color: inherit;
+          font-size: .875rem;
+          border-top: 1px solid ${theme.palette.border};
+          border-bottom-left-radius: ${theme.layout.radius};
+          border-bottom-right-radius: ${theme.layout.radius};
+          min-height: calc(2.5 * ${theme.layout.gap});
+        }
+        
+        .auto-margin :global(*) {
+          margin-top: 0;
+          margin-bottom: 0;
+          margin-right: ${theme.layout.gapQuarter};
+        }
+      `}</style>
+    </footer>
+  )
+}
+
+export default withDefaults(CardFooter, defaultProps)
