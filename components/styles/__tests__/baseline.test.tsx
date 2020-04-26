563844e3aa91b75d765962a6ca1f57291facd19c
unix <unix.bio@gmail.com>
2020-04-26 21:06:41 +0800

    test(styles): add testcase

---
+++ components/styles/__tests__/baseline.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render } from 'enzyme'
+import { CSSBaseline, ZEITUIProvider } from 'components'
+
+describe('CSSBaseline', () => {
+  it('should render correctly', () => {
+    const wrapper = render(
+      <ZEITUIProvider>
+        <CSSBaseline />
+      </ZEITUIProvider>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+  it('should render dark mode correctly', () => {
+    const wrapper = render(
+      <ZEITUIProvider theme={{ type: 'dark' }}>
+        <CSSBaseline />
+      </ZEITUIProvider>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
