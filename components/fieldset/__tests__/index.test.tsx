0820950e6e48d3c93355efd178f343f2ea02d3d1
unix <unix.bio@gmail.com>
2020-04-29 01:44:55 +0800

    test(fieldset): add testcase for fieldset.content

--- components/fieldset/__tests__/index.test.tsx
+++ components/fieldset/__tests__/index.test.tsx
@@ -30,4 +30,36 @@ describe('Fieldset', () => {
     )
     expect(wrapper).toMatchSnapshot()
   })
+  
+  it('the component Fieldset.Content should be injected automatically', () => {
+    const fieldset = mount(
+      <Fieldset>
+        <Fieldset.Title>test-title</Fieldset.Title>
+        <Fieldset.Subtitle>test-subtitle</Fieldset.Subtitle>
+      </Fieldset>
+    )
+    const content = mount(
+      <Fieldset>
+        <Fieldset.Content>
+          <Fieldset.Title>test-title</Fieldset.Title>
+          <Fieldset.Subtitle>test-subtitle</Fieldset.Subtitle>
+        </Fieldset.Content>
+      </Fieldset>
+    )
+    
+    expect(fieldset.html()).toEqual(content.html())
+    expect(() => content.unmount()).not.toThrow()
+  })
+  
+  it('render children through props', () => {
+    const fieldset = mount(
+      <Fieldset>
+        <Fieldset.Title>test-title</Fieldset.Title>
+        <Fieldset.Subtitle>test-subtitle</Fieldset.Subtitle>
+      </Fieldset>
+    )
+    const props = mount(<Fieldset title="test-title" subtitle="test-subtitle" />)
+    expect(fieldset.html()).toEqual(props.html())
+    expect(() => props.unmount()).not.toThrow()
+  })
 })
