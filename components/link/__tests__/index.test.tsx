909cee6ae1bd89c5ff9fce2dd2092c118b1a27b1
unix <unix.bio@gmail.com>
2020-05-08 16:58:51 +0800

    test(link): add testcase for deprecate warning

--- components/link/__tests__/index.test.tsx
+++ components/link/__tests__/index.test.tsx
@@ -41,5 +41,13 @@ describe('Link', () => {
     expect(errorSpy).not.toHaveBeenCalled()
     expect(ref.current).not.toBeNull()
     expect(() => wrapper.unmount()).not.toThrow()
+    errorSpy.mockRestore()
+  })
+
+  it('an warning should be thrown when using the pure prop', () => {
+    const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
+    mount(<Link pure />)
+    expect(errorSpy).toHaveBeenCalled()
+    errorSpy.mockRestore()
   })
 })
