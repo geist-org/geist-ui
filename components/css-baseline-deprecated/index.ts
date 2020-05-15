cf19e6a05de01df766306942ad3873b25e79fc91
unix <unix.bio@gmail.com>
2020-05-16 01:38:32 +0800

    chore: remove unnecessary warnings of deprecated

--- components/css-baseline-deprecated/index.ts
+++
@@ -1,6 +0,0 @@
-import CssBaseline from '../css-baseline'
-import useWarning from '../utils/use-warning'
-
-useWarning('Module "CSSBaseline" is deprecated. Use `CssBaseline` instead of it.', 'CSSBaseline')
-
-export default CssBaseline
