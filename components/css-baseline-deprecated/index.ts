b93acc8d75e7ec53e0ec0edd307db7ad71214702
unix <unix.bio@gmail.com>
2020-05-15 22:38:30 +0800

    refactor: rename the modules to make sure tree-shaking works

---
+++ components/css-baseline-deprecated/index.ts
@@ -0,0 +1,6 @@
+import CssBaseline from '../css-baseline'
+import useWarning from '../utils/use-warning'
+
+useWarning('Module "CSSBaseline" is deprecated. Use `CssBaseline` instead of it.', 'CSSBaseline')
+
+export default CssBaseline
---
+++ components/zeit-provider-deprecated/index.ts
@@ -0,0 +1,9 @@
+import ZeitProvider from '../zeit-provider'
+import useWarning from '../utils/use-warning'
+
+useWarning(
+  'Module "ZEITUIProvider" is deprecated. Use `ZeitProvider` instead of it.',
+  'ZEITUIProvider',
+)
+
+export default ZeitProvider
