67ee18e5615a3eff3f503ab6a6b4ca3c79810b19
unix <unix.bio@gmail.com>
2020-05-30 21:44:44 +0800

    feat(breadcrumbs): add component
    docs(breadcrumbs): add docs
    
    fix(breadcrumbs): fix item types error

---
+++ components/breadcrumbs/index.ts
@@ -0,0 +1,8 @@
+import Breadcrumbs from './breadcrumbs'
+import BreadcrumbsItem from './breadcrumbs-item'
+import BreadcrumbsSeparator from './breadcrumbs-separator'
+
+Breadcrumbs.Item = BreadcrumbsItem
+Breadcrumbs.Separator = BreadcrumbsSeparator
+
+export default Breadcrumbs
