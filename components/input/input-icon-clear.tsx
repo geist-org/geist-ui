b39b25eb33875a9e9656f2f360be589734f71e05
yqrashawn <namy.19@gmail.com>
2020-07-10 15:11:35 +0800

    feat(input): change input clear icon

--- components/input/input-icon-clear.tsx
+++ components/input/input-icon-clear.tsx
@@ -21,18 +21,12 @@ const InputIconClear: React.FC<Props> = ({ onClick, heightRatio, disabled, visib
   }
   return (
     <div onClick={clickHandler} className={`clear-icon ${visibale ? 'visibale' : ''}`}>
-      <svg
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill="none"
-        shapeRendering="geometricPrecision">
-        <path d="M18 6L6 18" />
-        <path d="M6 6l12 12" />
+      <svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M7 1.75C4.1125 1.75 1.75 4.1125 1.75 7C1.75 9.8875 4.1125 12.25 7 12.25C9.8875 12.25 12.25 9.8875 12.25 7C12.25 4.1125 9.8875 1.75 7 1.75ZM9.29688 8.37812L8.37812 9.29688L7 7.91875L5.62188 9.29688L4.70312 8.37812L6.08125 7L4.70312 5.62188L5.62188 4.70312L7 6.08125L8.37812 4.70312L9.29688 5.62188L7.91875 7L9.29688 8.37812Z"
+          fill="currentColor"
+        />
       </svg>
-
       <style jsx>{`
         .clear-icon {
           padding: 0 ${theme.layout.gapHalf};
@@ -43,7 +37,7 @@ const InputIconClear: React.FC<Props> = ({ onClick, heightRatio, disabled, visib
           cursor: ${disabled ? 'not-allowed' : 'pointer'};
           box-sizing: border-box;
           transition: color 150ms ease 0s;
-          color: ${theme.palette.accents_3};
+          color: ${theme.palette.cGray2};
           visibility: hidden;
           opacity: 0;
         }
