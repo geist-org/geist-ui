232ef3cefd75ee78088b2b76a12eaae56bbe3277
yqrashawn <namy.19@gmail.com>
2020-07-20 11:42:47 +0800

    fix: add default export for test icon

--- pages/en-us/components/test-icons.tsx
+++ pages/en-us/components/test-icons.tsx
@@ -1,4 +1,4 @@
-export const InfoIcon = () => (
+const InfoIcon = () => (
   <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M7.00033 12.8333C10.222 12.8333 12.8337 10.2216 12.8337 6.99996C12.8337 3.7783 10.222 1.16663 7.00033 1.16663C3.77866 1.16663 1.16699 3.7783 1.16699 6.99996C1.16699 10.2216 3.77866 12.8333 7.00033 12.8333Z"
@@ -10,3 +10,5 @@ export const InfoIcon = () => (
     <path d="M7 4.66663H7.00583" stroke="#282D30" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 )
+
+export default InfoIcon
