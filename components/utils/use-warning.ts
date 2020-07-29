7426f9b55c889e5377d7dc0ff5b486e35fea1af3
jnoodle <fullstack@126.com>
2020-07-29 10:12:01 +0800

    feat(utils): add level support to use-warning component

--- components/utils/use-warning.ts
+++ components/utils/use-warning.ts
@@ -1,12 +1,18 @@
 const warningStack: { [key: string]: boolean } = {}
 
-const useWarning = (message: string, component?: string) => {
+const useWarning = (
+  message: string,
+  component?: string,
+  level?: 'error' | 'warn' | 'info' | 'log',
+) => {
   const tag = component ? ` [${component}]` : ' '
-  const log = `[Zeit UI]${tag}: ${message}`
+  const log = `[@cfxjs/react-ui]${tag}: ${message}`
 
   if (typeof console === 'undefined') return
   if (warningStack[log]) return
   warningStack[log] = true
+
+  if (level) return console[level](log)
 
   if (process.env.NODE_ENV !== 'production') {
     return console.error(log)
