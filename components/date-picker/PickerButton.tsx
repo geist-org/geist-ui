746dfcb4eec74e36f26f4f14c0406abe8f9027e9
jnoodle <fullstack@126.com>
2020-08-05 16:32:42 +0800

    refactor(date-picker): follow the naming convention

--- components/date-picker/PickerButton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import Button from '../button'
-import { ButtonProps } from '../button/button'
-
-// confirm button for time picker
-export default function PickerButton(props: Partial<ButtonProps>) {
-  return <Button size="small" color="primary" {...props} />
-}
--- components/date-picker/PickerTag.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import Tag, { Props } from '../tag'
-
-// tag style for range picker `ranges` prop
-export default function PickerTag(props: Partial<Props>) {
-  return <Tag type="default" className="tag" {...props} />
-}
