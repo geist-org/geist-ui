bb25fef31a9b7bcc9084a66df8c869d137b45652
jnoodle <fullstack@126.com>
2020-08-24 11:15:03 +0800

    feat(date-picker): replace moment to dayjs

--- components/date-picker/index.tsx
+++ components/date-picker/index.tsx
@@ -1,21 +1,21 @@
-import { Moment } from 'moment'
-import momentGenerateConfig from '@jnoodle/rc-picker/lib/generate/moment'
+import { Dayjs } from 'dayjs'
+import generateConfig from '@jnoodle/rc-picker/lib/generate/dayjs'
 import generatePicker, {
   PickerProps,
   PickerDateProps,
   RangePickerProps as BaseRangePickerProps,
 } from './generate-picker'
 
-export type DatePickerProps = PickerProps<Moment>
-export type MonthPickerProps = Omit<PickerDateProps<Moment>, 'picker'>
-export type WeekPickerProps = Omit<PickerDateProps<Moment>, 'picker'>
-export type RangePickerProps = BaseRangePickerProps<Moment>
+export type DatePickerProps = PickerProps<Dayjs>
+export type MonthPickerProps = Omit<PickerDateProps<Dayjs>, 'picker'>
+export type WeekPickerProps = Omit<PickerDateProps<Dayjs>, 'picker'>
+export type RangePickerProps = BaseRangePickerProps<Dayjs>
 
 // TODO Fix range picker start week class name bug - @jnoodle/rc-picker
 // TODO Mobile Compatibility:
 //      It's acceptable now, but the experience is not excellent.
 //      Think about `<input type="date/time" />`
 
-export const DatePicker = generatePicker<Moment>(momentGenerateConfig)
+export const DatePicker = generatePicker<Dayjs>(generateConfig)
 
 export default DatePicker
