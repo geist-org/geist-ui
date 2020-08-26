7a1aa2778f2a977689526c68f72d374d2f30b36a
jnoodle <fullstack@126.com>
2020-08-26 18:18:33 +0800

    fix(date-picker): useDatePickerHandle methods

--- components/date-picker/use-date-picker-handle.ts
+++ components/date-picker/use-date-picker-handle.ts
@@ -1,14 +1,15 @@
 import { useRef } from 'react'
 import { Dayjs } from 'dayjs'
 import { PickerRefConfig } from '@jnoodle/rc-picker/es/Picker'
+import { RangeValue } from '@jnoodle/rc-picker/es/interface'
 
 export default () => {
   const ref = useRef<PickerRefConfig<Dayjs>>(null)
   return {
-    focus: ref.current?.focus,
-    blur: ref.current?.blur,
-    setValue: ref.current?.setValue,
-    getValue: ref.current?.getValue,
+    focus: () => ref.current?.focus(),
+    blur: () => ref.current?.blur(),
+    setValue: (val: Dayjs | null | RangeValue<Dayjs>) => ref.current?.setValue(val),
+    getValue: () => ref.current?.getValue(),
     ref,
   }
 }
