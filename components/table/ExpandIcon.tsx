6c4107ae2eee2b4a4caf8edf70db95f778786715
yqrashawn <namy.19@gmail.com>
2020-08-28 11:29:59 +0800

    fix(table): no need to import *

--- components/table/ExpandIcon.tsx
+++ components/table/ExpandIcon.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 
 interface DefaultExpandIconProps<RecordType> {
   onExpand: (record: RecordType, e: React.MouseEvent<HTMLElement>) => void
