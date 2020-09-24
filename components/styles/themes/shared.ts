54d904020e7cfdacf0529989b9cbbd8f394f51a8
witt <unix.bio@gmail.com>
2020-09-24 13:57:35 +0800

    feat: optimize fonts rendering on windows (#385)
    * feat(styles): set Inter to highest font
    
    * docs(fonts): add guide for fonts rendering on windows
    
    * test: udpate snapshots

--- components/styles/themes/shared.ts
+++ components/styles/themes/shared.ts
@@ -6,7 +6,7 @@ import {
 
 export const defaultFont: GeistUIThemesFont = {
   sans:
-    '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+    '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
   mono:
     'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace',
 }
