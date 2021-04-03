4b042169f216405e8a3228f84b36f305960cc214
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-03 18:31:30 +0530

    test(footer): reduce snapshot file size

--- components/footer/__tests__/index.test.tsx
+++ components/footer/__tests__/index.test.tsx
@@ -8,20 +8,9 @@ describe('should render basic footer', () => {
       <Footer>
         <Footer.Group title="Company">
           <Footer.Link href="#">Home</Footer.Link>
-          <Footer.Link href="#">About</Footer.Link>
-          <Footer.Link href="#">Careers</Footer.Link>
-          <Footer.Link href="#">Partners</Footer.Link>
-          <Footer.Link href="#">Blog</Footer.Link>
-          <Footer.Link href="#">Next.js Conf</Footer.Link>
         </Footer.Group>
         <Footer.Group title="Product">
           <Footer.Link href="#">Pricing</Footer.Link>
-          <Footer.Link href="#">Vercel for GitHub</Footer.Link>
-          <Footer.Link href="#">Vercel for GitLab</Footer.Link>
-          <Footer.Link href="#">Vercel for Bitbucket</Footer.Link>
-          <Footer.Link href="#">Vercel Edge Network</Footer.Link>
-          <Footer.Link href="#">Integrations Marketplace</Footer.Link>
-          <Footer.Link href="#">Command-Line</Footer.Link>
         </Footer.Group>
       </Footer>,
     )
@@ -33,12 +22,9 @@ describe('should render basic footer', () => {
       <Footer breakPoint="800px" maxWidth="900px">
         <Footer.Group title="Education">
           <Footer.Link href="#">Documentation</Footer.Link>
-          <Footer.Link href="#">Guides</Footer.Link>
-          <Footer.Link href="#">Support</Footer.Link>
         </Footer.Group>
         <Footer.Group title="More">
           <Footer.Link href="#">Open Source Software</Footer.Link>
-          <Footer.Link href="#">Design System</Footer.Link>
         </Footer.Group>
       </Footer>,
     )
@@ -49,13 +35,10 @@ describe('should render basic footer', () => {
     const wrapper = mount(
       <Footer light>
         <Footer.Group title="Education">
-          <Footer.Link href="#">Documentation</Footer.Link>
-          <Footer.Link href="#">Guides</Footer.Link>
           <Footer.Link href="#">Support</Footer.Link>
         </Footer.Group>
         <Footer.Group title="More">
           <Footer.Link href="#">Open Source Software</Footer.Link>
-          <Footer.Link href="#">Design System</Footer.Link>
         </Footer.Group>
       </Footer>,
     )
@@ -67,12 +50,9 @@ describe('should render basic footer', () => {
       <Footer light subFooter="<h1>Sub-Footer</h1>">
         <Footer.Group title="Education">
           <Footer.Link href="#">Documentation</Footer.Link>
-          <Footer.Link href="#">Guides</Footer.Link>
-          <Footer.Link href="#">Support</Footer.Link>
         </Footer.Group>
         <Footer.Group title="More">
           <Footer.Link href="#">Open Source Software</Footer.Link>
-          <Footer.Link href="#">Design System</Footer.Link>
         </Footer.Group>
       </Footer>,
     )
@@ -82,32 +62,15 @@ describe('should render basic footer', () => {
   it('should render multi-group columns', () => {
     const wrapper = render(
       <Footer subFooter={<Text h6>Copyright © Geist React. All rights reserved.</Text>}>
-        <Footer.Group title="Company">
-          <Footer.Link href="#">Home</Footer.Link>
-          <Footer.Link href="#">About</Footer.Link>
-          <Footer.Link href="#">Careers</Footer.Link>
-          <Footer.Link href="#">Partners</Footer.Link>
-          <Footer.Link href="#">Blog</Footer.Link>
-          <Footer.Link href="#">Next.js Conf</Footer.Link>
-        </Footer.Group>
         <Footer.Group title="Product">
           <Footer.Link href="#">Pricing</Footer.Link>
-          <Footer.Link href="#">Vercel for GitHub</Footer.Link>
-          <Footer.Link href="#">Vercel for GitLab</Footer.Link>
-          <Footer.Link href="#">Vercel for Bitbucket</Footer.Link>
-          <Footer.Link href="#">Vercel Edge Network</Footer.Link>
-          <Footer.Link href="#">Integrations Marketplace</Footer.Link>
-          <Footer.Link href="#">Command-Line</Footer.Link>
         </Footer.Group>
         <Footer.Column>
           <Footer.Group title="Education">
             <Footer.Link href="#">Documentation</Footer.Link>
-            <Footer.Link href="#">Guides</Footer.Link>
-            <Footer.Link href="#">Support</Footer.Link>
           </Footer.Group>
           <Footer.Group title="More">
             <Footer.Link href="#">Open Source Software</Footer.Link>
-            <Footer.Link href="#">Design System</Footer.Link>
           </Footer.Group>
         </Footer.Column>
       </Footer>,
