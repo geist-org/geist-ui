66b0c891e8481781ffbf0ff18cabdcc9bfd1d9dd
izayl <izayl@163.com>
2021-04-11 22:16:55 +0800

    fix: fieldset title/subtitle use block element (#510)
    * fix: fieldset title/subtitle use block element
    
    * test: update test-snapshot

--- components/fieldset/fieldset-title.tsx
+++ components/fieldset/fieldset-title.tsx
@@ -9,7 +9,7 @@ const defaultProps = {
   className: '',
 }
 
-type NativeAttrs = Omit<React.HTMLAttributes<HTMLHeadingElement>, keyof Props>
+type NativeAttrs = Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
 export type FieldsetTitleProps = Props & typeof defaultProps & NativeAttrs
 
 const FieldsetTitle: React.FC<FieldsetTitleProps> = ({
@@ -19,16 +19,18 @@ const FieldsetTitle: React.FC<FieldsetTitleProps> = ({
 }) => {
   return (
     <>
-      <h4 className={className} {...props}>
+      <div className={className} {...props}>
         {children}
-      </h4>
+      </div>
       <style jsx>{`
-        h4 {
+        div {
           font-size: 1.25rem;
           line-height: 1.5;
           margin: 0;
           display: inline-flex;
           word-break: break-word;
+          font-weight: 600;
+          letter-spacing: -0.020625rem;
         }
       `}</style>
     </>
