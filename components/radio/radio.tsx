52700fb20ffc2459a62c7567d4643a1f094d4655
gepd <guillermoepd@hotmail.com>
2021-04-28 17:00:25 -0400

    fix: radio not exposing the ref property

--- components/radio/radio.tsx
+++ components/radio/radio.tsx
@@ -1,4 +1,12 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, {
+  PropsWithoutRef,
+  RefAttributes,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import useTheme from '../use-theme'
 import { useRadioContext } from './radio-context'
 import RadioGroup, { getRadioSize } from './radio-group'
@@ -36,146 +44,156 @@ const defaultProps = {
 type NativeAttrs = Omit<React.InputHTMLAttributes<any>, keyof Props>
 export type RadioProps = Props & typeof defaultProps & NativeAttrs
 
-const Radio: React.FC<React.PropsWithChildren<RadioProps>> = ({
-  className,
-  checked,
-  onChange,
-  disabled,
-  size,
-  value: radioValue,
-  children,
-  ...props
-}) => {
-  const theme = useTheme()
-  const [selfChecked, setSelfChecked] = useState<boolean>(!!checked)
-  const { value: groupValue, disabledAll, inGroup, updateState } = useRadioContext()
-  const [withoutDescChildren, DescChildren] = pickChild(children, RadioDescription)
+const Radio = React.forwardRef<HTMLInputElement, React.PropsWithChildren<RadioProps>>(
+  (
+    {
+      className,
+      checked,
+      onChange,
+      disabled,
+      size,
+      value: radioValue,
+      children,
+      ...props
+    },
+    ref: React.Ref<HTMLInputElement | null>,
+  ) => {
+    const theme = useTheme()
+    const [selfChecked, setSelfChecked] = useState<boolean>(!!checked)
+    const { value: groupValue, disabledAll, inGroup, updateState } = useRadioContext()
+    const [withoutDescChildren, DescChildren] = pickChild(children, RadioDescription)
+    const inputRef = useRef<HTMLInputElement>(null)
+    useImperativeHandle(ref, () => inputRef.current)
 
-  if (inGroup) {
-    if (checked !== undefined) {
-      useWarning('Remove props "checked" if in the Radio.Group.', 'Radio')
-    }
-    if (radioValue === undefined) {
-      useWarning('Props "value" must be deinfed if in the Radio.Group.', 'Radio')
-    }
-    useEffect(() => {
-      setSelfChecked(groupValue === radioValue)
-    }, [groupValue, radioValue])
-  }
-
-  const fontSize = useMemo(() => getRadioSize(size), [size])
-  const isDisabled = useMemo(() => disabled || disabledAll, [disabled, disabledAll])
-  const changeHandler = (event: React.ChangeEvent) => {
-    if (isDisabled) return
-    const selfEvent: RadioEvent = {
-      target: {
-        checked: !selfChecked,
-      },
-      stopPropagation: event.stopPropagation,
-      preventDefault: event.preventDefault,
-      nativeEvent: event,
-    }
-    setSelfChecked(!selfChecked)
     if (inGroup) {
-      updateState && updateState(radioValue as string | number)
+      if (checked !== undefined) {
+        useWarning('Remove props "checked" if in the Radio.Group.', 'Radio')
+      }
+      if (radioValue === undefined) {
+        useWarning('Props "value" must be deinfed if in the Radio.Group.', 'Radio')
+      }
+      useEffect(() => {
+        setSelfChecked(groupValue === radioValue)
+      }, [groupValue, radioValue])
     }
-    onChange && onChange(selfEvent)
-  }
 
-  useEffect(() => {
-    if (checked === undefined) return
-    setSelfChecked(Boolean(checked))
-  }, [checked])
+    const fontSize = useMemo(() => getRadioSize(size), [size])
+    const isDisabled = useMemo(() => disabled || disabledAll, [disabled, disabledAll])
+    const changeHandler = (event: React.ChangeEvent) => {
+      if (isDisabled) return
+      const selfEvent: RadioEvent = {
+        target: {
+          checked: !selfChecked,
+        },
+        stopPropagation: event.stopPropagation,
+        preventDefault: event.preventDefault,
+        nativeEvent: event,
+      }
+      setSelfChecked(!selfChecked)
+      if (inGroup) {
+        updateState && updateState(radioValue as string | number)
+      }
+      onChange && onChange(selfEvent)
+    }
 
-  return (
-    <div className={`radio ${className}`}>
-      <label>
-        <input
-          type="radio"
-          value={radioValue}
-          checked={selfChecked}
-          onChange={changeHandler}
-          {...props}
-        />
-        <span className="name">
-          <span className={`point ${selfChecked ? 'active' : ''}`} />
-          {withoutDescChildren}
-        </span>
-        {DescChildren && DescChildren}
-      </label>
+    useEffect(() => {
+      if (checked === undefined) return
+      setSelfChecked(Boolean(checked))
+    }, [checked])
 
-      <style jsx>{`
-        input {
-          opacity: 0;
-          visibility: hidden;
-          overflow: hidden;
-          width: 1px;
-          height: 1px;
-          top: -1000px;
-          right: -1000px;
-          position: fixed;
-        }
+    return (
+      <div className={`radio ${className}`}>
+        <label>
+          <input
+            type="radio"
+            value={radioValue}
+            checked={selfChecked}
+            onChange={changeHandler}
+            ref={inputRef}
+            {...props}
+          />
+          <span className="name">
+            <span className={`point ${selfChecked ? 'active' : ''}`} />
+            {withoutDescChildren}
+          </span>
+          {DescChildren && DescChildren}
+        </label>
 
-        .radio {
-          display: flex;
-          width: initial;
-          align-items: flex-start;
-          position: relative;
-          --radio-size: ${fontSize};
-        }
+        <style jsx>{`
+          input {
+            opacity: 0;
+            visibility: hidden;
+            overflow: hidden;
+            width: 1px;
+            height: 1px;
+            top: -1000px;
+            right: -1000px;
+            position: fixed;
+          }
 
-        label {
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-start;
-          color: ${isDisabled ? theme.palette.accents_4 : theme.palette.foreground};
-          cursor: ${isDisabled ? 'not-allowed' : 'pointer'};
-        }
+          .radio {
+            display: flex;
+            width: initial;
+            align-items: flex-start;
+            position: relative;
+            --radio-size: ${fontSize};
+          }
 
-        .name {
-          font-size: var(--radio-size);
-          font-weight: bold;
-          user-select: none;
-          display: inline-flex;
-          align-items: center;
-        }
+          label {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            color: ${isDisabled ? theme.palette.accents_4 : theme.palette.foreground};
+            cursor: ${isDisabled ? 'not-allowed' : 'pointer'};
+          }
 
-        .point {
-          height: var(--radio-size);
-          width: var(--radio-size);
-          border-radius: 50%;
-          border: 1px solid ${theme.palette.border};
-          transition: all 0.2s ease 0s;
-          position: relative;
-          display: inline-block;
-          transform: scale(0.875);
-          margin-right: calc(var(--radio-size) * 0.375);
-        }
+          .name {
+            font-size: var(--radio-size);
+            font-weight: bold;
+            user-select: none;
+            display: inline-flex;
+            align-items: center;
+          }
 
-        .point:before {
-          content: '';
-          position: absolute;
-          left: -1px;
-          top: -1px;
-          transform: scale(0);
-          height: var(--radio-size);
-          width: var(--radio-size);
-          border-radius: 50%;
-          background-color: ${isDisabled
-            ? theme.palette.accents_4
-            : theme.palette.foreground};
-        }
+          .point {
+            height: var(--radio-size);
+            width: var(--radio-size);
+            border-radius: 50%;
+            border: 1px solid ${theme.palette.border};
+            transition: all 0.2s ease 0s;
+            position: relative;
+            display: inline-block;
+            transform: scale(0.875);
+            margin-right: calc(var(--radio-size) * 0.375);
+          }
 
-        .point.active:before {
-          transform: scale(0.875);
-          transition: all 0.2s ease 0s;
-        }
-      `}</style>
-    </div>
-  )
-}
+          .point:before {
+            content: '';
+            position: absolute;
+            left: -1px;
+            top: -1px;
+            transform: scale(0);
+            height: var(--radio-size);
+            width: var(--radio-size);
+            border-radius: 50%;
+            background-color: ${isDisabled
+              ? theme.palette.accents_4
+              : theme.palette.foreground};
+          }
 
-type RadioComponent<P = {}> = React.FC<P> & {
+          .point.active:before {
+            transform: scale(0.875);
+            transition: all 0.2s ease 0s;
+          }
+        `}</style>
+      </div>
+    )
+  },
+)
+
+type RadioComponent<T, P = {}> = React.ForwardRefExoticComponent<
+  PropsWithoutRef<P> & RefAttributes<T>
+> & {
   Group: typeof RadioGroup
   Desc: typeof RadioDescription
   Description: typeof RadioDescription
@@ -186,4 +204,4 @@ type ComponentProps = Partial<typeof defaultProps> &
 
 Radio.defaultProps = defaultProps
 
-export default Radio as RadioComponent<ComponentProps>
+export default Radio as RadioComponent<HTMLInputElement, ComponentProps>
