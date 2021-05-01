0750e430196aa37d345f5b3793c581519820c2f6
gepd <guillermoepd@hotmail.com>
2021-04-30 23:29:08 -0400

    fix: auto-complete not exposing ref

--- components/auto-complete/auto-complete.tsx
+++ components/auto-complete/auto-complete.tsx
@@ -1,4 +1,13 @@
-import React, { CSSProperties, useEffect, useMemo, useRef, useState } from 'react'
+import React, {
+  CSSProperties,
+  PropsWithoutRef,
+  RefAttributes,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import Input from '../input'
 import AutoCompleteItem, { AutoCompleteItemProps } from './auto-complete-item'
 import AutoCompleteDropdown from './auto-complete-dropdown'
@@ -71,160 +80,171 @@ const getSearchIcon = (searching?: boolean) => {
   return searching ? <Loading size="medium" /> : <span />
 }
 
-const AutoComplete: React.FC<React.PropsWithChildren<AutoCompleteProps>> = ({
-  options,
-  initialValue: customInitialValue,
-  onSelect,
-  onSearch,
-  onChange,
-  searching,
-  children,
-  size,
-  status,
-  value,
-  width,
-  clearable,
-  disabled,
-  dropdownClassName,
-  dropdownStyle,
-  disableMatchWidth,
-  disableFreeSolo,
-  ...props
-}) => {
-  const ref = useRef<HTMLDivElement>(null)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const resetTimer = useRef<number>()
-  const [state, setState, stateRef] = useCurrentState<string>(customInitialValue)
-  const [selectVal, setSelectVal] = useState<string>(customInitialValue)
-  const [visible, setVisible] = useState<boolean>(false)
-
-  const [, searchChild] = pickChild(children, AutoCompleteSearching)
-  const [, emptyChild] = pickChild(children, AutoCompleteEmpty)
-  const autoCompleteItems = useMemo(() => {
-    const hasSearchChild = searchChild && React.Children.count(searchChild) > 0
-    const hasEmptyChild = emptyChild && React.Children.count(emptyChild) > 0
-    if (searching) {
-      return hasSearchChild ? (
-        searchChild
-      ) : (
-        <AutoCompleteSearching>Searching...</AutoCompleteSearching>
-      )
-    }
-    if (options.length === 0) {
-      if (state === '') return null
-      return hasEmptyChild ? (
-        emptyChild
-      ) : (
-        <AutoCompleteEmpty>No Options</AutoCompleteEmpty>
-      )
-    }
-    return childrenToOptionsNode(options as Array<AutoCompleteOption>)
-  }, [searching, options])
-  const showClearIcon = useMemo(() => clearable && searching === undefined, [
-    clearable,
-    searching,
-  ])
-
-  const updateValue = (val: string) => {
-    if (disabled) return
-    setSelectVal(val)
-    onSelect && onSelect(val)
-    setState(val)
-    inputRef.current && inputRef.current.focus()
-  }
-  const updateVisible = (next: boolean) => setVisible(next)
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setVisible(true)
-    onSearch && onSearch(event.target.value)
-    setState(event.target.value)
-  }
-  const resetInputValue = () => {
-    if (!disableFreeSolo) return
-    if (!state || state === '') return
-    if (state !== selectVal) {
-      setState(selectVal)
-    }
-  }
-
-  useEffect(() => {
-    onChange && onChange(state)
-  }, [state])
-  useEffect(() => {
-    if (value === undefined) return
-    setState(value)
-  }, [value])
-
-  const initialValue = useMemo<AutoCompleteConfig>(
-    () => ({
-      ref,
+const AutoComplete = React.forwardRef<
+  HTMLInputElement,
+  React.PropsWithChildren<AutoCompleteProps>
+>(
+  (
+    {
+      options,
+      initialValue: customInitialValue,
+      onSelect,
+      onSearch,
+      onChange,
+      searching,
+      children,
       size,
-      value: state,
-      updateValue,
-      visible,
-      updateVisible,
-    }),
-    [state, visible, size],
-  )
+      status,
+      value,
+      width,
+      clearable,
+      disabled,
+      dropdownClassName,
+      dropdownStyle,
+      disableMatchWidth,
+      disableFreeSolo,
+      ...props
+    },
+    ref: React.Ref<HTMLInputElement | null>,
+  ) => {
+    const divRef = useRef<HTMLDivElement>(null)
+    const inputRef = useRef<HTMLInputElement>(null)
+    const resetTimer = useRef<number>()
+    const [state, setState, stateRef] = useCurrentState<string>(customInitialValue)
+    const [selectVal, setSelectVal] = useState<string>(customInitialValue)
+    const [visible, setVisible] = useState<boolean>(false)
+    useImperativeHandle(ref, () => inputRef.current)
 
-  const toggleFocusHandler = (next: boolean) => {
-    clearTimeout(resetTimer.current)
-    setVisible(next)
-    if (next) {
-      onSearch && onSearch(stateRef.current)
-    } else {
-      resetTimer.current = window.setTimeout(() => {
-        resetInputValue()
-        clearTimeout(resetTimer.current)
-      }, 100)
+    const [, searchChild] = pickChild(children, AutoCompleteSearching)
+    const [, emptyChild] = pickChild(children, AutoCompleteEmpty)
+    const autoCompleteItems = useMemo(() => {
+      const hasSearchChild = searchChild && React.Children.count(searchChild) > 0
+      const hasEmptyChild = emptyChild && React.Children.count(emptyChild) > 0
+      if (searching) {
+        return hasSearchChild ? (
+          searchChild
+        ) : (
+          <AutoCompleteSearching>Searching...</AutoCompleteSearching>
+        )
+      }
+      if (options.length === 0) {
+        if (state === '') return null
+        return hasEmptyChild ? (
+          emptyChild
+        ) : (
+          <AutoCompleteEmpty>No Options</AutoCompleteEmpty>
+        )
+      }
+      return childrenToOptionsNode(options as Array<AutoCompleteOption>)
+    }, [searching, options])
+    const showClearIcon = useMemo(() => clearable && searching === undefined, [
+      clearable,
+      searching,
+    ])
+
+    const updateValue = (val: string) => {
+      if (disabled) return
+      setSelectVal(val)
+      onSelect && onSelect(val)
+      setState(val)
+      inputRef.current && inputRef.current.focus()
     }
-  }
+    const updateVisible = (next: boolean) => setVisible(next)
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setVisible(true)
+      onSearch && onSearch(event.target.value)
+      setState(event.target.value)
+    }
+    const resetInputValue = () => {
+      if (!disableFreeSolo) return
+      if (!state || state === '') return
+      if (state !== selectVal) {
+        setState(selectVal)
+      }
+    }
 
-  const inputProps = {
-    ...props,
-    width,
-    disabled,
-    value: state,
-  }
+    useEffect(() => {
+      onChange && onChange(state)
+    }, [state])
+    useEffect(() => {
+      if (value === undefined) return
+      setState(value)
+    }, [value])
 
-  return (
-    <AutoCompleteContext.Provider value={initialValue}>
-      <div ref={ref} className="auto-complete">
-        <Input
-          ref={inputRef}
-          size={size}
-          status={status}
-          onChange={onInputChange}
-          onFocus={() => toggleFocusHandler(true)}
-          onBlur={() => toggleFocusHandler(false)}
-          clearable={showClearIcon}
-          iconRight={getSearchIcon(searching)}
-          {...inputProps}
-        />
-        <AutoCompleteDropdown
-          visible={visible}
-          disableMatchWidth={disableMatchWidth}
-          className={dropdownClassName}
-          dropdownStyle={dropdownStyle}>
-          {autoCompleteItems}
-        </AutoCompleteDropdown>
+    const initialValue = useMemo<AutoCompleteConfig>(
+      () => ({
+        ref: divRef,
+        size,
+        value: state,
+        updateValue,
+        visible,
+        updateVisible,
+      }),
+      [state, visible, size],
+    )
 
-        <style jsx>{`
-          .auto-complete {
-            width: ${width || 'max-content'};
-          }
+    const toggleFocusHandler = (next: boolean) => {
+      clearTimeout(resetTimer.current)
+      setVisible(next)
+      if (next) {
+        onSearch && onSearch(stateRef.current)
+      } else {
+        resetTimer.current = window.setTimeout(() => {
+          resetInputValue()
+          clearTimeout(resetTimer.current)
+        }, 100)
+      }
+    }
 
-          .auto-complete :global(.loading) {
-            left: -3px;
-            right: -3px;
-            width: max-content;
-          }
-        `}</style>
-      </div>
-    </AutoCompleteContext.Provider>
-  )
-}
+    const inputProps = {
+      ...props,
+      width,
+      disabled,
+      value: state,
+    }
 
-type AutoCompleteComponent<P = {}> = React.FC<P> & {
+    return (
+      <AutoCompleteContext.Provider value={initialValue}>
+        <div ref={divRef} className="auto-complete">
+          <Input
+            ref={inputRef}
+            size={size}
+            status={status}
+            onChange={onInputChange}
+            onFocus={() => toggleFocusHandler(true)}
+            onBlur={() => toggleFocusHandler(false)}
+            clearable={showClearIcon}
+            iconRight={getSearchIcon(searching)}
+            {...inputProps}
+          />
+          <AutoCompleteDropdown
+            visible={visible}
+            disableMatchWidth={disableMatchWidth}
+            className={dropdownClassName}
+            dropdownStyle={dropdownStyle}>
+            {autoCompleteItems}
+          </AutoCompleteDropdown>
+
+          <style jsx>{`
+            .auto-complete {
+              width: ${width || 'max-content'};
+            }
+
+            .auto-complete :global(.loading) {
+              left: -3px;
+              right: -3px;
+              width: max-content;
+            }
+          `}</style>
+        </div>
+      </AutoCompleteContext.Provider>
+    )
+  },
+)
+
+type AutoCompleteComponent<T, P = {}> = React.ForwardRefExoticComponent<
+  PropsWithoutRef<P> & RefAttributes<T>
+> & {
   Item: typeof AutoCompleteItem
   Option: typeof AutoCompleteItem
   Searching: typeof AutoCompleteSearching
@@ -234,6 +254,9 @@ type AutoCompleteComponent<P = {}> = React.FC<P> & {
 type ComponentProps = Partial<typeof defaultProps> &
   Omit<Props, keyof typeof defaultProps> &
   NativeAttrs
-;(AutoComplete as AutoCompleteComponent<ComponentProps>).defaultProps = defaultProps
+;(AutoComplete as AutoCompleteComponent<
+  HTMLInputElement,
+  ComponentProps
+>).defaultProps = defaultProps
 
-export default AutoComplete as AutoCompleteComponent<ComponentProps>
+export default AutoComplete as AutoCompleteComponent<HTMLInputElement, ComponentProps>
