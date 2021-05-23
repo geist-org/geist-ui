8394c362ec9b7059d39e36976e5d7ecb36cf99d2
unix <unix.bio@gmail.com>
2021-05-24 00:12:28 +0800

    feat(usekeyboard): redesign event handler to match keyboard events from browser
    \

--- components/use-keyboard/codes.ts
+++ components/use-keyboard/codes.ts
@@ -1,7 +1,6 @@
 /**
  * KeyBinding Codes
  * The content of this file is based on the design of the open source project "microsoft/vscode",
- *   Ref: https://github.com/microsoft/vscode/blob/main/src/vs/base/common/keyCodes.ts (MIT)
  *   Copyright (c) Microsoft Corporation. All rights reserved.
  *
  * We inherit the KeyMod values from "microsoft/vscode",
@@ -10,172 +9,81 @@
 
 export enum KeyCode {
   Unknown = 0,
-  Backspace = 1,
-  Tab = 2,
-  Enter = 3,
-  PauseBreak = 7,
-  CapsLock = 8,
-  Escape = 9,
-  Space = 10,
-  PageUp = 11,
-  PageDown = 12,
-  End = 13,
-  Home = 14,
-  LeftArrow = 15,
-  UpArrow = 16,
-  RightArrow = 17,
-  DownArrow = 18,
-  Insert = 19,
-  Delete = 20,
-  KEY_0 = 21,
-  KEY_1 = 22,
-  KEY_2 = 23,
-  KEY_3 = 24,
-  KEY_4 = 25,
-  KEY_5 = 26,
-  KEY_6 = 27,
-  KEY_7 = 28,
-  KEY_8 = 29,
-  KEY_9 = 30,
-  KEY_A = 31,
-  KEY_B = 32,
-  KEY_C = 33,
-  KEY_D = 34,
-  KEY_E = 35,
-  KEY_F = 36,
-  KEY_G = 37,
-  KEY_H = 38,
-  KEY_I = 39,
-  KEY_J = 40,
-  KEY_K = 41,
-  KEY_L = 42,
-  KEY_M = 43,
-  KEY_N = 44,
-  KEY_O = 45,
-  KEY_P = 46,
-  KEY_Q = 47,
-  KEY_R = 48,
-  KEY_S = 49,
-  KEY_T = 50,
-  KEY_U = 51,
-  KEY_V = 52,
-  KEY_W = 53,
-  KEY_X = 54,
-  KEY_Y = 55,
-  KEY_Z = 56,
-  Meta = 57,
-  ContextMenu = 58,
-  F1 = 59,
-  F2 = 60,
-  F3 = 61,
-  F4 = 62,
-  F5 = 63,
-  F6 = 64,
-  F7 = 65,
-  F8 = 66,
-  F9 = 67,
-  F10 = 68,
-  F11 = 69,
-  F12 = 70,
-  F13 = 71,
-  F14 = 72,
-  F15 = 73,
-  F16 = 74,
-  F17 = 75,
-  F18 = 76,
-  F19 = 77,
-  NumLock = 78,
-  ScrollLock = 79,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   * For the US standard keyboard, the ';:' key
-   */
-  US_SEMICOLON = 80,
-  /**
-   * For any country/region, the '+' key
-   * For the US standard keyboard, the '=+' key
-   */
-  US_EQUAL = 81,
-  /**
-   * For any country/region, the ',' key
-   * For the US standard keyboard, the ',<' key
-   */
-  US_COMMA = 82,
-  /**
-   * For any country/region, the '-' key
-   * For the US standard keyboard, the '-_' key
-   */
-  US_MINUS = 83,
-  /**
-   * For any country/region, the '.' key
-   * For the US standard keyboard, the '.>' key
-   */
-  US_DOT = 84,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   * For the US standard keyboard, the '/?' key
-   */
-  US_SLASH = 85,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   * For the US standard keyboard, the '`~' key
-   */
-  US_BACKTICK = 86,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   * For the US standard keyboard, the '[{' key
-   */
-  US_OPEN_SQUARE_BRACKET = 87,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   * For the US standard keyboard, the '\|' key
-   */
-  US_BACKSLASH = 88,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   * For the US standard keyboard, the ']}' key
-   */
-  US_CLOSE_SQUARE_BRACKET = 89,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   * For the US standard keyboard, the ''"' key
-   */
-  US_QUOTE = 90,
-  /**
-   * Used for miscellaneous characters; it can vary by keyboard.
-   */
-  OEM_8 = 91,
-  /**
-   * Either the angle bracket key or the backslash key on the RT 102-key keyboard.
-   */
-  OEM_102 = 92,
-  NUMPAD_0 = 93,
-  NUMPAD_1 = 94,
-  NUMPAD_2 = 95,
-  NUMPAD_3 = 96,
-  NUMPAD_4 = 97,
-  NUMPAD_5 = 98,
-  NUMPAD_6 = 99,
-  NUMPAD_7 = 100,
-  NUMPAD_8 = 101,
-  NUMPAD_9 = 102,
-  NUMPAD_MULTIPLY = 103,
-  NUMPAD_ADD = 104,
-  NUMPAD_SEPARATOR = 105,
-  NUMPAD_SUBTRACT = 106,
-  NUMPAD_DECIMAL = 107,
-  NUMPAD_DIVIDE = 108,
-  /**
-   * Cover all key codes when IME is processing input.
-   */
-  KEY_IN_COMPOSITION = 109,
-  ABNT_C1 = 110,
-  ABNT_C2 = 111,
-  /**
-   * Placed last to cover the length of the enum.
-   * Please do not depend on this value!
-   */
-  MAX_VALUE = 112,
+  Backspace = 8,
+  Tab = 9,
+  Enter = 13,
+  Shift = 16,
+  Ctrl = 17,
+  Alt = 18,
+  PauseBreak = 19,
+  CapsLock = 20,
+  Escape = 27,
+  Space = 32,
+  PageUp = 33,
+  PageDown = 34,
+  End = 35,
+  Home = 36,
+  LeftArrow = 37,
+  UpArrow = 38,
+  RightArrow = 39,
+  DownArrow = 40,
+  Insert = 45,
+  Delete = 46,
+  KEY_0 = 48,
+  KEY_1 = 49,
+  KEY_2 = 50,
+  KEY_3 = 51,
+  KEY_4 = 52,
+  KEY_5 = 53,
+  KEY_6 = 54,
+  KEY_7 = 55,
+  KEY_8 = 56,
+  KEY_9 = 57,
+  KEY_A = 65,
+  KEY_B = 66,
+  KEY_C = 67,
+  KEY_D = 68,
+  KEY_E = 69,
+  KEY_F = 70,
+  KEY_G = 71,
+  KEY_H = 72,
+  KEY_I = 73,
+  KEY_J = 74,
+  KEY_K = 75,
+  KEY_L = 76,
+  KEY_M = 77,
+  KEY_N = 78,
+  KEY_O = 79,
+  KEY_P = 80,
+  KEY_Q = 81,
+  KEY_R = 82,
+  KEY_S = 83,
+  KEY_T = 84,
+  KEY_U = 85,
+  KEY_V = 86,
+  KEY_W = 87,
+  KEY_X = 88,
+  KEY_Y = 89,
+  KEY_Z = 90,
+  Meta = 91,
+  F1 = 112,
+  F2 = 113,
+  F3 = 114,
+  F4 = 115,
+  F5 = 116,
+  F6 = 117,
+  F7 = 118,
+  F8 = 119,
+  F9 = 120,
+  F10 = 121,
+  F11 = 122,
+  F12 = 123,
+  NumLock = 144,
+  ScrollLock = 145,
+  Equal = 187,
+  Minus = 189,
+  Backquote = 192,
+  Backslash = 220,
 }
 
 export enum KeyMod {
--- components/use-keyboard/use-keyboard.ts
+++ components/use-keyboard/use-keyboard.ts
@@ -1,123 +1,78 @@
 import { KeyMod } from './codes'
 import React, { useEffect } from 'react'
-import { isMac } from '../utils/collections'
-
-export type KeyBindings = Array<number>
+import { getActiveModMap, getCtrlKeysByPlatform } from './helper'
 
 export type KeyboardOptions = {
-  globalEvent?: boolean
+  disableGlobalEvent?: boolean
+  stopPropagation?: boolean
   preventDefault?: boolean
   capture?: boolean
   event?: 'keydown' | 'keypress' | 'keyup'
 }
 
-export type KeyboardResult<T> = {
+export type KeyboardResult = {
   bindings: {
-    onKeyDown: React.KeyboardEventHandler<T>
-    onKeyDownCapture: React.KeyboardEventHandler<T>
-    onKeyPress: React.KeyboardEventHandler<T>
-    onKeyPressCapture: React.KeyboardEventHandler<T>
-    onKeyUp: React.KeyboardEventHandler<T>
-    onKeyUpCapture: React.KeyboardEventHandler<T>
+    onKeyDown: React.KeyboardEventHandler
+    onKeyDownCapture: React.KeyboardEventHandler
+    onKeyPress: React.KeyboardEventHandler
+    onKeyPressCapture: React.KeyboardEventHandler
+    onKeyUp: React.KeyboardEventHandler
+    onKeyUpCapture: React.KeyboardEventHandler
   }
 }
 
-export type UseKeyboardHandler = (
-  event: React.KeyboardEvent<Element> | KeyboardEvent,
-) => void
+export type UseKeyboardHandler = (event: React.KeyboardEvent | KeyboardEvent) => void
 
-export type UseKeyboard =
-  | ((
-      keyBindings: KeyBindings,
-      options?: KeyboardOptions,
-      handler?: UseKeyboardHandler,
-    ) => KeyboardResult<Element>)
-  | ((keyBindings: KeyBindings, handler?: UseKeyboardHandler) => KeyboardResult<Element>)
+export type UseKeyboard = (
+  handler: UseKeyboardHandler,
+  keyBindings: Array<number> | number,
+  options?: KeyboardOptions,
+) => KeyboardResult
 
-const getActiveModMap = (bindings: KeyBindings = []) => {
-  const activeModMap: Record<keyof typeof KeyMod, boolean> = {
-    CtrlCmd: false,
-    Shift: false,
-    Alt: false,
-    WinCtrl: false,
-  }
-  for (const key of bindings) {
-    const val = bindings[key]
-    const modKey = KeyMod[val] as keyof typeof KeyMod | undefined
-    if (modKey) {
-      activeModMap[modKey] = true
-    }
-  }
-  return activeModMap
-}
-
-const useKeyboard: UseKeyboard = (keyBindings, optionsOrHandler, handler) => {
-  const options =
-    typeof optionsOrHandler === 'function' ? {} : (optionsOrHandler as KeyboardOptions)
-  const userHandler =
-    typeof optionsOrHandler === 'function'
-      ? (optionsOrHandler as UseKeyboardHandler)
-      : handler
+const useKeyboard: UseKeyboard = (handler, keyBindings, options = {}) => {
+  const bindings = Array.isArray(keyBindings) ? (keyBindings as number[]) : [keyBindings]
   const {
-    globalEvent = true,
+    disableGlobalEvent = false,
     capture = false,
+    stopPropagation = false,
     preventDefault = true,
-    event = 'keypress',
+    event = 'keydown',
   } = options
-  const modBindings = keyBindings.filter((item: number) => !!KeyMod[item])
-  const keyCode = keyBindings.filter((item: number) => !KeyMod[item])[0]
-  const activeModMap = getActiveModMap(modBindings)
+  const activeModMap = getActiveModMap(bindings)
+  const keyCode = bindings.filter((item: number) => !KeyMod[item])[0]
+  const { CtrlCmd, WinCtrl } = getCtrlKeysByPlatform()
 
   const eventHandler = (event: React.KeyboardEvent | KeyboardEvent) => {
-    if (activeModMap.Shift) {
-      if (!event.shiftKey) return
-    }
-    if (activeModMap.Alt) {
-      if (!event.altKey) return
-    }
-    if (activeModMap.CtrlCmd) {
-      if (isMac()) {
-        if (!event.metaKey) return
-      } else {
-        if (!event.ctrlKey) return
-      }
-    }
-    if (activeModMap.WinCtrl) {
-      if (isMac()) {
-        if (!event.ctrlKey) return
-      } else {
-        if (!event.metaKey) return
-      }
-    }
-    if (keyCode) {
-      if (event.keyCode !== keyCode) return
+    if (activeModMap.Shift && !event.shiftKey) return
+    if (activeModMap.Alt && !event.altKey) return
+    if (activeModMap.CtrlCmd && !event[CtrlCmd]) return
+    if (activeModMap.WinCtrl && !event[WinCtrl]) return
+    if (keyCode && event.keyCode !== keyCode) return
+    if (stopPropagation) {
+      event.stopPropagation()
     }
     if (preventDefault) {
       event.preventDefault()
     }
-    userHandler && userHandler(event)
-    return !preventDefault
+    handler && handler(event)
   }
 
   useEffect(() => {
-    if (globalEvent) {
+    if (!disableGlobalEvent) {
       document.addEventListener(event, eventHandler)
     }
     return () => {
       document.removeEventListener(event, eventHandler)
     }
-  }, [globalEvent])
+  }, [disableGlobalEvent])
 
   const elementBindingHandler = (
     elementEventType: 'keydown' | 'keypress' | 'keyup',
     isCapture: boolean = false,
   ) => {
-    if (globalEvent) return () => {}
     if (elementEventType !== event) return () => {}
     if (isCapture !== capture) return () => {}
-    return (e: React.KeyboardEvent) => {
-      eventHandler(e)
-    }
+    return (e: React.KeyboardEvent) => eventHandler(e)
   }
 
   return {
