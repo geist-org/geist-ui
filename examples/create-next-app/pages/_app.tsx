fb3256ce0f6db7b87ce794cc39977ffa48393d58
witt <unix.bio@gmail.com>
2021-06-24 00:50:04 +0800

    chore(examples): upgrade next examples for scaleable (#564)

--- examples/create-next-app/pages/_app.tsx
+++ examples/create-next-app/pages/_app.tsx
@@ -1,6 +1,7 @@
+import type { AppProps } from 'next/app'
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GeistProvider>
       <CssBaseline />
