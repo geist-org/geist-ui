29f3f6bad11d9f1536f34f68414cd55e90da9540
witt <unix.bio@gmail.com>
2022-01-23 02:48:07 +0800

    chore: simplify the type srouce of third-party lib (#683)
    * chore: simplify the type srouce of third-party lib
    
    * chore: release v2.2.5

--- typings/styled.d.ts
+++ typings/styled.d.ts
@@ -1,9 +1,10 @@
-declare global {
-  declare module 'react' {
-    interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
-      jsx?: boolean;
-      global?: boolean;
-    }
+// Definitions by: @types/styled-jsx <https://www.npmjs.com/package/@types/styled-jsx>
+
+import 'react'
+
+declare module 'react' {
+  interface StyleHTMLAttributes<T> extends HTMLAttributes<T> {
+    jsx?: boolean
+    global?: boolean
   }
 }
-
