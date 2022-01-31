f63bcce7789e9ae53179f147ede81e2d29a5d3b1
unix <unix.bio@gmail.com>
2022-02-01 01:38:17 +0800

    docs: skip title format when on hooks page

--- lib/components/layout/header.tsx
+++ lib/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Head from 'next/head'
 
 export interface Meta {
@@ -10,10 +10,17 @@ const toCapitalize = (name: string) => {
   return `${first.toUpperCase()}${rest.join('')}`
 }
 
-const PageHeader: React.FC<{ meta: Meta }> = ({ meta }) => (
-  <Head>
-    <title>{meta.title ? `${toCapitalize(meta.title)} - ` : ''} Geist UI</title>
-  </Head>
-)
+const PageHeader: React.FC<{ meta: Meta }> = ({ meta }) => {
+  const capitalizeTitle = useMemo(() => {
+    if (!meta.title) return ''
+    if (meta.title.toLowerCase().startsWith('use')) return `${meta.title} - `
+    return `${toCapitalize(meta.title)} - `
+  }, [meta.title])
+  return (
+    <Head>
+      <title>{capitalizeTitle}Geist UI</title>
+    </Head>
+  )
+}
 
 export default PageHeader
