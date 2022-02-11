5365c8bf3988a5554e205cb7d995fd8c5a07f197
unix <unix.bio@gmail.com>
2022-02-11 20:42:44 +0800

    docs: remove inter font

---
+++ scripts/side-effects.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const fs = require('fs-extra')
+
+;(async () => {
+  const pkgPath = path.join(__dirname, '../package.json')
+  const json = await fs.readJson(pkgPath)
+  json.sideEffects = true
+  await fs.writeJson(pkgPath, json, { spaces: 2 })
+})()
