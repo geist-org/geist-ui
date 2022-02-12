6462d1737c56b1c2c6f836389644d5324a191028
Hoon <dt@live.it>
2022-02-12 22:19:47 +0800

    fix(card): remove fixed width of card content

--- components/card/card-content.tsx
+++ components/card/card-content.tsx
@@ -25,7 +25,6 @@ const CardContentComponent: React.FC<React.PropsWithChildren<CardContentProps>> 
       {children}
       <style jsx>{`
         .content {
-          width: ${SCALES.width(1, '100%')};
           height: ${SCALES.height(1, 'auto')};
           padding: ${SCALES.pt(1)} ${SCALES.pr(1)} ${SCALES.pb(1)} ${SCALES.pl(1)};
           margin: ${SCALES.mt(0)} ${SCALES.mr(0)} ${SCALES.mb(0)} ${SCALES.ml(0)};
