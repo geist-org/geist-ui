2ffe97ace896eb9b0b3e2d239f7c2da2b95000af
colinricardo <colin@colin.fun>
2022-11-21 15:37:08 +0100

    feat: add sortableName to file tree

--- components/tree/tree.tsx
+++ components/tree/tree.tsx
@@ -1,10 +1,10 @@
 import React, { useMemo } from 'react'
+import useClasses from '../use-classes'
+import { tuple } from '../utils/prop-types'
+import { TreeContext } from './tree-context'
 import TreeFile from './tree-file'
 import TreeFolder from './tree-folder'
-import { TreeContext } from './tree-context'
-import { tuple } from '../utils/prop-types'
 import { sortChildren } from './tree-help'
-import useClasses from '../use-classes'
 
 const FileTreeValueType = tuple('directory', 'file')
 
@@ -13,6 +13,7 @@ const directoryType = FileTreeValueType[0]
 export type TreeFile = {
   type: typeof FileTreeValueType[number]
   name: string
+  sortableName?: string
   extra?: string
   files?: Array<TreeFile>
 }
@@ -37,6 +38,10 @@ const makeChildren = (value: Array<TreeFile> = []) => {
   return value
     .sort((a, b) => {
       if (a.type !== b.type) return a.type !== directoryType ? 1 : -1
+
+      if (a.sortableName && b.sortableName) {
+        return `${a.sortableName}`.charCodeAt(0) - `${b.sortableName}`.charCodeAt(0)
+      }
 
       return `${a.name}`.charCodeAt(0) - `${b.name}`.charCodeAt(0)
     })
